3bf5ceae5f5ee3e7aa8b0fb24f8497669dbcca05
youngxhui <youngxhui@gmail.com>
2021-04-19 19:38:19 +0800

    完成tools获取

--- entity/tool.go
+++ entity/tool.go
@@ -2,14 +2,23 @@ package entity
 
 import (
 	"boot/db"
-	"boot/protos"
 	"gorm.io/gorm"
+)
+
+const (
+	// Health 健康
+	Health = 0
+	// Waring 即将损坏
+	Waring = 1
+	// Damage 已经损坏
+	Damage = 2
 )
 
 // Tool 刀具信息
 type Tool struct {
 	gorm.Model
-	protos.Tool
+	MachineId int
+	Status    int
 }
 
 func init() {
@@ -18,5 +27,16 @@ func init() {
 
 func (t *Tool) FindById() *Tool {
 	db.DB.First(&t, t.ID)
+	if t.ID == 0 {
+		return nil
+	}
 	return t
+}
+
+// FindAllTools 获取所有的Tools
+func FindAllTools(page int) []Tool {
+	offset := (page - 1) * 10
+	tools := make([]Tool,10)
+	db.DB.Limit(10).Offset(offset).Find(&tools)
+	return tools
 }
--- srv/tool_srv.go
+++ srv/tool_srv.go
@@ -4,6 +4,8 @@ import (
 	"boot/entity"
 	pd "boot/protos"
 	"context"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/emptypb"
 	"gorm.io/gorm"
 )
@@ -11,9 +13,22 @@ import (
 type ToolService struct {
 }
 
+// ListTools 获取 10 个数据
 func (t *ToolService) ListTools(ctx context.Context, in *pd.ListToolsRequest) (*pd.ListToolsResponse, error) {
-	return nil, nil
+	size := in.PageSize
+	tools := entity.FindAllTools(int(size))
+	pdTools := make([]*pd.Tool,10)
+	for i := 0; i < len(tools); i++ {
+		pdTools[i] = &pd.Tool{
+			Id:        int32(tools[i].ID),
+			MachineId: int32(tools[i].MachineId),
+			Status: pd.Status(tools[i].Status),
+		}
+	}
+	return &pd.ListToolsResponse{Tools: pdTools}, nil
 }
+
+// GetTool 通过 id 获取刀具状态
 func (t *ToolService) GetTool(ctx context.Context, in *pd.GetToolRequest) (*pd.Tool, error) {
 	tool := entity.Tool{
 		Model: gorm.Model{
@@ -21,13 +36,16 @@ func (t *ToolService) GetTool(ctx context.Context, in *pd.GetToolRequest) (*pd.T
 		},
 	}
 	toolFirst := tool.FindById()
-
+	if toolFirst == nil {
+		return nil, status.Error(codes.ResourceExhausted, "没有该刀具信息")
+	}
 	return &pd.Tool{
-		Id:        toolFirst.Id,
-		MachineId: toolFirst.MachineId,
-		Status:    toolFirst.Status,
+		Id:        int32(toolFirst.ID),
+		MachineId: int32(toolFirst.MachineId),
+		Status:    pd.Status(toolFirst.Status),
 	}, nil
 }
+
 func (t *ToolService) CreateTool(ctx context.Context, in *pd.CreateToolRequest) (*pd.Tool, error) {
 	return nil, nil
 }
