2f91b5c0e215b167ca70904ba8b54982edb08a73
youngxhui <youngxhui@gmail.com>
2021-04-27 20:49:08 +0800

    完成消息推送

--- websocket/tool_ws.go
+++ websocket/tool_ws.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"boot/protos"
 	"fmt"
+	"github.com/gorilla/websocket"
+	"google.golang.org/protobuf/proto"
 	"log"
 	"math/rand"
 	"net/http"
-	"strconv"
 	"time"
 )
 
@@ -18,29 +20,47 @@ func tool(w http.ResponseWriter, r *http.Request) {
 	}
 
 	defer c.Close()
-	mt, message, err := c.ReadMessage()
+	_, message, err := c.ReadMessage()
+	wear := new(protos.Wear)
+	if err = proto.Unmarshal(message, wear); err != nil {
+		log.Println(err.Error())
+	}
 	for {
 
 		if err != nil {
 			log.Println("read:", err)
 			break
 		}
-		log.Printf("recv: %s", message)
-		id, err := strconv.Atoi(string(message))
-		if err != nil {
-			log.Println("err :", err)
-		}
-		log.Println("id", id)
-
 		f := rand.Float32() * 100
+		//str := fmt.Sprintf("%.0f", f)
+		if f > 80 {
+			go errorNotify(c, int(wear.Id))
+		} else if f > 60 {
+			go waringNotify(c, int(wear.Id))
+		}
 
-		str := fmt.Sprintf("%.0f", f)
-		err = c.WriteMessage(mt, []byte(str))
-		//fmt.Println(runtime.)
+		wear := protos.Wear{
+			Id:     wear.Id,
+			ToolId: 1,
+			Date:   time.Now().Format("15:04:05"),
+			Loss:   f,
+		}
+		marshal, err := proto.Marshal(&wear)
+		err = c.WriteMessage(websocket.BinaryMessage, marshal)
 		if err != nil {
 			fmt.Println("write:", err)
 			break
 		}
 		time.Sleep(1 * time.Second)
 	}
+}
+
+// 警告信息⚠
+func errorNotify(c *websocket.Conn, id int) {
+
+}
+
+// 错误信息❌
+func waringNotify(c *websocket.Conn, id int) {
+
 }
